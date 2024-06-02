87f423f4cd3e018c5484a727307d32fe446af5f4
gipsoong <ryangipsoong@gmail.com>
2024-06-02 18:18:52 +0800

    Create Item class and initialize its attributes. Create basic methods to return attributes.

---
+++ items/item.py
@@ -0,0 +1,25 @@
+class Item:
+    def __init__(self, title, category, language, authors, year_published):
+        self.title = title
+        self.category = category
+        self.language = language
+        self.authors = authors
+        self.year_published = year_published
+
+    def return_title(self):
+        return self.title
+
+    def return_category(self):
+        return self.category
+
+    def return_language(self):
+        return self.language
+
+    def return_authors(self):
+        return self.authors
+
+    def return_year_published(self):
+        return self.year_published
+
+    def __str__(self):
+        return True
