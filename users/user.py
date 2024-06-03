d004884e12271ec4da9a47e111823df9aa2465c3
gipsoong <ryangipsoong@gmail.com>
2024-06-04 02:22:49 +0800

    Create basic methods for the User class.

--- users/user.py
+++ users/user.py
@@ -6,3 +6,15 @@ class User:
         self.last_name = last_name
         self.username = username
         self.password = password
+
+    def return_first_name(self):
+        return self.first_name
+
+    def return_last_name(self):
+        return self.last_name
+
+    def return_username(self):
+        return self.first_name
+
+    def return_password(self):
+        return self.password
