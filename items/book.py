6f9f632d1feb07db71a269fa8daddaa6ae7a8698
gipsoong <ryangipsoong@gmail.com>
2024-06-04 02:28:58 +0800

    Create 'Book' class that is a child of 'Item' and initialize its variables.

---
+++ items/book.py
@@ -0,0 +1,12 @@
+from item import Item
+
+
+class Book(Item):
+    def __init__(self, title, category, language, authors, year_published):
+        super().__init__(title, category, language, authors, year_published)
+
+        self.title = title
+        self.category = category
+        self.language = language
+        self.authors = authors
+        self.year_published = year_published
