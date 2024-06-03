299f5cd6c0ddb171f45758cc9211b5d72dc1e576
gipsoong <ryangipsoong@gmail.com>
2024-06-04 02:24:51 +0800

    Conduct minor formatting. Make the password attribute private.

--- users/user.py
+++ users/user.py
@@ -1,11 +1,10 @@
 
-
 class User:
     def __init__(self, first_name, last_name, username, password):
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
-        self.password = password
+        self._password = password
 
     def return_first_name(self):
         return self.first_name
@@ -17,4 +16,4 @@ class User:
         return self.first_name
 
     def return_password(self):
-        return self.password
+        return self._password
