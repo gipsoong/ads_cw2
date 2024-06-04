eef6501aa3491cc518ce63fbcd41c60e41ca1c0c
gipsoong <ryangipsoong@gmail.com>
2024-06-05 02:06:03 +0800

    Create 'BorrowedItems' class and initialize its attributes.

---
+++ borrowed_items.py
@@ -0,0 +1,8 @@
+
+class BorrowedItems:
+
+    def __init__(self, borrowed_items, borrower_account_number, item_borrowed, date_borrowed):
+        self.borrowed_items = borrowed_items
+        self.borrower_account_number = borrower_account_number
+        self.item_borrowed = item_borrowed
+        self.date_borrowed = date_borrowed
