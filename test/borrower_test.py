7e949a9b4593027280b939440bd3f8b9cb753a34
gipsoong <ryangipsoong@gmail.com>
2024-06-06 00:40:44 +0800

    Modify project file structure. Create a test file for 'Borrower', import relevant classes and create a sample of a 'Borrower'.

---
+++ test/borrower_test.py
@@ -0,0 +1,17 @@
+from borrower import Borrower
+from book import Book
+
+
+def main():
+    book1 = Book('Metamorphosis', 'Self Help', 'English',
+                 'Franz Kafka', 1915, 10)
+
+    book2 = Book('Mein Kampf', 'Autobiography', 'German',
+                 'Adolf Hitler', 1915, 12)
+
+    jane_doe = Borrower('Jane', 'Doe', 'janedoe2', 'password123',
+                        800, [book1, book2])
+
+    return jane_doe.borrowed_items
+
+main()
