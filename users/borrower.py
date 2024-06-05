9c9a85ffa63e49f8581fb4f98aa05c08455dc8e4
gipsoong <ryangipsoong@gmail.com>
2024-06-06 00:19:44 +0800

    Update 'Borrower' class to take 'borrowed_items' instead of 'rented_items'. Modify methods accordingly.

--- users/borrower.py
+++ users/borrower.py
@@ -3,14 +3,13 @@ from user import User
 
 class Borrower(User):
 
-    def __init__(self, first_name, last_name, username, password, account_number, rented_items):
+    def __init__(self, first_name, last_name, username, password, account_number, borrowed_items):
         super().__init__(first_name, last_name, username, password)
-
         self.account_number = account_number
-        self.rented_items = rented_items
+        self.borrowed_items = borrowed_items
 
     def return_account_number(self):
         return self.account_number
 
-    def return_rented_items(self):
-        return self.rented_items
+    def borrowed_items(self):
+        return self.borrowed_items()
