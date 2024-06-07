fba9713b28776b1a7b5c72b55f715923689b9e53
gipsoong <ryangipsoong@gmail.com>
2024-06-08 02:07:38 +0800

    Create 'Tree' class and initialize its attribtues. Create a method for Tree to add a node.

--- library_tree.py
+++ library_tree.py
@@ -1,7 +1,14 @@
 
-
 class Node:
     def __init__(self, key):
         self.left = None
         self.right = None
         self.val = key
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def add(self, value):
+        if not self.root:
+            self.root = Node(value)
