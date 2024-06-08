09a4a53142948f3037073c2c6cda53e2236fe0fe
gipsoong <ryangipsoong@gmail.com>
2024-06-09 02:15:13 +0800

    Rename 'value' to 'key' for the 'Node' class.

--- library_tree.py
+++ library_tree.py
@@ -1,43 +1,43 @@
 class Node:
 
-    def __init__(self, value):
+    def __init__(self, key):
         self.left = None
         self.right = None
-        self.value = value
+        self.key = key
 
-    def insert(self, value):
-        if value < self.value:
+    def insert(self, key):
+        if key < self.key:
             if self.left is None:
-                self.left = Node(value)
+                self.left = Node(key)
             else:
-                self.left.insert(value)
+                self.left.insert(key)
 
         else:
             if self.right is None:
-                self.right = Node(value)
+                self.right = Node(key)
             else:
-                self.right.insert(value)
+                self.right.insert(key)
 
     def inorder_traversal(self):
         pass
         if self.left:
             self.left.inorder_traversal()
-        print(self.value)
+        print(self.key)
 
         if self.right:
             self.right.inorder_traversal()
 
-    def find(self, value):
-        if value < self.value:
+    def find(self, key):
+        if key < self.key:
             if self.left is None:
                 return False
             else:
-                return self.left.find(value)
+                return self.left.find(key)
 
-        elif value > self.value:
+        elif key > self.key:
             if self.right is None:
                 return False
             else:
-                return self.right.find(value)
+                return self.right.find(key)
         else:
             return True
