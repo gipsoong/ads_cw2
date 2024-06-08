17db42d0c8b1141fa9867a458696da481def91ff
gipsoong <ryangipsoong@gmail.com>
2024-06-09 03:05:58 +0800

    Add 'value' attribute to 'Node' class to accept a value in addition to the key.

--- library_tree.py
+++ library_tree.py
@@ -1,43 +1,37 @@
 class Node:
 
-    def __init__(self, key):
+    def __init__(self, key, value):
         self.left = None
         self.right = None
         self.key = key
+        self.value = value
 
-    def insert(self, key):
+    def insert(self, key, value=None):
         if key < self.key:
             if self.left is None:
                 self.left = Node(key)
+                self.left.value = value
             else:
-                self.left.insert(key)
+                self.left.insert(key, value)
 
         else:
             if self.right is None:
                 self.right = Node(key)
+                self.right.content = value
             else:
-                self.right.insert(key)
-
-    def inorder_traversal(self):
-        pass
-        if self.left:
-            self.left.inorder_traversal()
-        print(self.key)
-
-        if self.right:
-            self.right.inorder_traversal()
+                self.right.insert(key, value)
 
     def find(self, key):
         if key < self.key:
             if self.left is None:
-                return False
+                return None
             else:
                 return self.left.find(key)
 
         elif key > self.key:
             if self.right is None:
-                return False
+                return None
             else:
                 return self.right.find(key)
         else:
-            return True
+            return self
